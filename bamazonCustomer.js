a40ae6786354a8db6e965275bfe659a473baa3ce
Mitu Fikir <hannatefera2@gmail.com>
2019-09-14 17:16:18 -0700

    Finalized bamoazonCustomer.js code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,3 +23,59 @@ function displayProduct() {
         promptCustomer(res);
     })
 }
+
+function promptCustomer(res) {
+    inquirer.prompt([{
+        type: "input",
+        name: "choice",
+        message: "What would you like to purchase (select by id)?    [Exit with E]"
+    }]).then(function(answer) {
+        var reply = false;
+        if (answer.choice.toUpperCase() === "E") {
+            process.exit();
+        }
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].item_id == answer.choice || res[i].product_name === answer.choice) {
+                reply = true;
+                var chosenId = answer.choice;
+                var num = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quantity",
+                    message: "How many would you like to purchase?",
+                    validate: function(value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function(answer) {
+                    var balance = res[num].stock_quantity - answer.quantity;
+                    if (balance > 0) {
+                        var total = answer.quantity * res[num].price;
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?", [{
+                                    stock_quantity: balance
+                                },
+                                {
+                                    item_id: chosenId
+                                }
+                            ],
+                            function(err, result) {
+                                console.log("Your total cost is $" + total + "\n");
+                                displayProduct();
+                            })
+                    } else {
+                        console.log("Insufficient quantity");
+                        promptCustomer(res);
+                    }
+                })
+            }
+        }
+        if (i == res.length && reply == false) {
+            console.log("Not a valid selection!");
+            promptCustomer(res);
+        }
+    })
+}
